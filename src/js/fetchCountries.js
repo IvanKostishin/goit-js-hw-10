74a40d9c2a1c65c8a404af72c0ca7519a7966ef2
IvanKostishin <112808887+IvanKostishin@users.noreply.github.com>
2023-03-01 13:48:40 +0200

    js

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+﻿export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .catch((e) => {
+            if (e.message) alert(e.message);
+        });
+};
